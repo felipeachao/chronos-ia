0a4340742d3e84bf6ace3d9c4b9abb25cc1e3e68
João Achão <felipe.achao@gmail.com>
2024-10-06 23:08:39 -0300

    fix: error in deploy of render because env port

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def get_weather_info():
     return jsonify({'message': 'Mensagem enviada com sucesso!', 'gpt_response': gpt_weather_response})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port)
